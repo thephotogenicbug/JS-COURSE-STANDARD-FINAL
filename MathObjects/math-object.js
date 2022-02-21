570b3c7e8960afcb9e297fe59897205d9157f5e5
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-21 11:37:26 +0530

    21-02-2022 : JS MathObject Example

---
+++ MathObjects/math-object.js
@@ -0,0 +1,15 @@
+// JavaScript Inbuilt Objects
+// Math is a built-in object that has properties and methods for mathematical constants and functions. 
+// It's not a function object.
+
+// purple color is methods
+// Blue color is property, property contains some kinda values
+let x = 3
+//Returns the absolute value of x
+console.log(Math.abs(x));
+console.log("--------------------------------")
+
+// Returns the smallest integer greater than or equal to y.
+// The Math.ceil() function always rounds a number up to the next largest integer.
+let y = 1.4
+console.log(Math.ceil(y))
